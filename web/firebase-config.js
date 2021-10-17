78ce26cd1ade3026fff450ea75830ea84dc7b3ef
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-10-18 00:50:40 +0200

    Intégration Firebase

---
+++ web/firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBaXw9rt9ulFOM1YTzcHWIk8KnlO9HrTtg",
+  authDomain: "lego-storage-93011.firebaseapp.com",
+  projectId: "lego-storage-93011",
+  storageBucket: "lego-storage-93011.appspot.com",
+  messagingSenderId: "305920059449",
+  appId: "1:305920059449:web:a0709bc8c15ab2ecc945e8",
+  measurementId: "G-C670D4X2EX",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
